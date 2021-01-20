3c35da63a7e03262e0c23ec1e837bb6173a1cfdb
Sven Giese <sven.giese@hpi.de>
2021-01-20 16:02:20 +0000

    Add writing of sequences to user specific output dir
    - add functions to store valid/invalid sequences for now
    - add a writer module to be refactored later (see #31/#32)
    - fix tests
    - rebase / use sequence.py module to add a to_valid function
    - change output behavior to only store vaid sequences

--- president/sequence.py
+++ president/sequence.py
@@ -2,6 +2,23 @@
 import tempfile
 
 
+def to_valid_upper(dna_seq):
+    """
+    Transform input sequence to valid output sequence by removing all non-ACGT characters with Ns.
+
+    Parameters
+    ----------
+    dna_seq, str
+              DNA sequence
+
+    Returns
+    -------
+    str, fixed DNA sequence
+    """
+    valid_nt = set(list("acgtACGT"))
+    return "".join([i.upper() if i in valid_nt else "N" for i in dna_seq])
+
+
 def preprocess(fasta):
     """Remove white spaces from IDs in a FASTA file.
 
